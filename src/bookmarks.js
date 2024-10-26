c5b68e0ed92ce0127e96364ae20f66cf5be72654
jdev082 <jdev082@jdev.eu.org>
2024-10-26 13:17:14 -0500

    chore: apply release patches to develop
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -20,6 +20,7 @@ function addBookmarkToBar(url, title) {
     bookmarkEl.innerHTML = `<img src='${img}'<h1>${title}</h1>`;
     bookmarkEl.onclick = () => {
         createTab(url);
+        toggleBookmarks();
     };
     bookmarkEl.classList.add('bookmark');
     bookmarksBar.appendChild(bookmarkEl);

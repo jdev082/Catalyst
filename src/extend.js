9d1f41e0cbdaeed2955edbcfa7904bcf3c80edd8
jdev082 <jdev082@jdev.eu.org>
2024-12-13 23:06:54 +0000

    feat(): implement extend api
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- src/extend.js
+++ src/extend.js
@@ -1,8 +1,10 @@
-var catalyst = {
+var catalyst = JSON.stringify({
     'tabs': {
         current: document.getElementsByClassName('active-tab'),
-        'native': native,
-        'extend': {
-            isCatalyst: true,
-        },
-    }};
+    },
+    'native': native,
+    'extend': {
+        isCatalyst: true,
+        backgrounds: native.getBackgrounds(),
+    }
+});
